576853ccf12ac809e073da9ee34711570b5855b2
Adrian PHilip Rios <adrianphiliprios@gmail.com>
2018-02-09 22:34:38 +0800

    Initial commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = "#fff";
+let defaultStyle = {
+  color: defaultTextColor
+}
+class Aggregate extends Component{
+  render() {
+    return(
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }  
+}
+
+
+
+class Filter extends Component{
+  render(){
+    return(
+      <div style={{color: defaultTextColor}}>
+        <img/>
+        <input type="text" name="" id=""/>
+        Filter
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component{
+  render(){
+    return(
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ASDF</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+
+        <Playlist />
+         <Playlist />
+          <Playlist />
+           <Playlist />
+      </div>
+    );
+  }
+}
+
+export default App;
